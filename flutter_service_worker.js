132d62823490f091f546213a389c8c30c478f595
Dewanik Koirala <dewanik>
2024-07-15 23:22:28 -0400

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4a56b6b254c4c40e6099d5b0d1bb4c14
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "766821d6c9fa7cf746ae892147d7a47c",
+"flutter_bootstrap.js": "dce794a01738662044fac31563f37ba6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
